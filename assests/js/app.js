c2c4a87b5152118bfb6ac48278f3be9f83a44265
siddiquesahar123 <126084762+siddiquesahar123@users.noreply.github.com>
2023-10-27 00:04:33 +0500

    Add files via upload

---
+++ assests/js/app.js
@@ -0,0 +1,16 @@
+const result  = document.getElementById('result');
+ 
+function display(number){
+    result.value += number;
+}
+function calculate() {
+    let finalNumber = result.value;
+    let finalResult = eval(finalNumber);
+    result.value = finalResult;
+}
+function clears(){
+    result.value = "";
+}
+function del(){
+    result.value = result.value.slice(0,-1);
+}
